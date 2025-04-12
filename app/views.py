aab013c2733dc1a9c7829e191b5a40877d59137b
prem068 <premsagar.068@gmail.com>
2025-04-12 21:44:29 +0530

    new

--- app/views.py
+++ app/views.py
@@ -3,7 +3,6 @@ from .models import Task
 from app.forms import *
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .models import Task
 from .serializers import TaskSerializer
 # Create your views here.
 
